600a80145dedc67dd50c7a6702dc44743a9c8229
Gabriel Herolt <103610441+h3rolt@users.noreply.github.com>
2022-06-14 12:42:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('.informacao-A').popover({
+        placement: 'bottom',
+        trigger: 'click',
+        html: true,
+        content: '<div class="popover-aberto"><img class="imagem-popover" src="modeloti.jpg" alt=""><p class="titulo-popover">Ciencias da Computação</p><p>O curso de Ciência da Computação forma profissionais qualificados para desenvolver programas e sistemas de informática, desde o planejamento do projeto até a implantação e gerenciamento do software. Por ser da área de ciências exatas, o aluno precisa ter uma boa base matemática e raciocínio lógico desenvolvido. Ao final do curso, o estudante poderá criar de ferramentas mais simples com computadores pessoais até sistemas complexos como os de processamento de informações e redes corporativas. No mercado de trabalho, o cientista da computação pode atuar como desenvolvedor, programador, analista de TI, arquiteto de software, gerente de TI ou pesquisador em instituições públicas ou privadas. O tipo de formação para o curso de Ciência da Computação é: bacharelado.</p></div>'
+    });
+});
+
+$(document).ready(function() {
+    $('.informacao-B').popover({
+        placement: 'bottom',
+        trigger: 'click',
+        html: true,
+        content: '<div class="popover-aberto"><img class="imagem-popover" src="modeloti.jpg" alt=""><p class="titulo-popover">Engenharia da Computação</p><p>O curso de Engenharia da Computação tem em sua grade curricular disciplinas voltadas para eletrônica e fabricação de hardware. Essa é a atuação mais tradicional da profissão. Nesse mercado, o engenheiro vai projetar e construir equipamentos, sejam eles computacionais ou periféricos, como teclados, mouses etc.</p></div>'
+    });
+});
+
+$(document).ready(function() {
+    $('.informacao-C').popover({
+        placement: 'bottom',
+        trigger: 'click',
+        html: true,
+        content: '<div class="popover-aberto"><img class="imagem-popover" src="modeloti.jpg" alt=""><p class="titulo-popover">Sistema da informação</p><p>São as disciplinas que incluem a programação de softwares, sites e aplicativos e as diversas plataformas de acesso à informação, como computadores e celulares. O curso de Sistemas de Informação é a porta de entrada para uma carreira de sucesso, no cargo de Analista de Sistemas da Informação se inicia ganhando R$ 3.128,00 de salário e pode vir a ganhar até R$ 6.528,00.</p></div>'
+    });
+});
+
+$(document).ready(function() {
+    $('.informacao-D').popover({
+        placement: 'bottom',
+        trigger: 'click',
+        html: true,
+        content: '<div class="popover-aberto"><img class="imagem-popover" src="modeloti.jpg" alt=""><p class="titulo-popover">Análise de sistemas</p><p>O analista de sistemas atua na área da Tecnologia da Informação desenvolvendo programas que atendam às necessidades específicas das organizações. Em outras palavras, esse profissional é responsável por desenvolver soluções para os problemas das empresas atuais por meio de sistemas informatizados.</p></div>'
+    });
+});
